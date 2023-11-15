bd08c4fea48a11b2b14f468ff3b07d6d37315636
nHuy101 <nhathuyho2k3@gmail.com>
2023-11-16 00:35:59 +0700

    User_Sidebar - create & append into UserLayout - Fix centering HeaderNav & Footer - Add route ( user/profile )

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -10,48 +10,50 @@ import visaIcon from '~/assets/images/visa.svg'
 export default function Footer() {
 	return (
 		<div className='flex flex-col font-roboto text-white tracking-wider'>
-			<div className='flex flex-row py-4 px-16 gap-14 bg-primary justify-evenly'>
-				<div className='flex flex-col mt-4'>
-					<p className='text-lg font-bold'>CHĂM SÓC KHÁCH HÀNG</p>
-					<div className='my-3 ml-6'>
-						<p>Điện thoại liên hệ: +84 344444404</p>
-						<p>Email: xxx@4food.com</p>
-						<p>Giờ làm việc: 7.30 AM - 9.30 PM</p>
+			<div className='bg-primary justify-evenly'>
+				<div className='w-primary mx-auto max-w-full flex flex-row py-4 px-16 gap-14'>
+					<div className='flex flex-col mt-4'>
+						<p className='text-lg font-bold'>CHĂM SÓC KHÁCH HÀNG</p>
+						<div className='my-3 ml-6'>
+							<p>Điện thoại liên hệ: +84 344444404</p>
+							<p>Email: xxx@4food.com</p>
+							<p>Giờ làm việc: 7.30 AM - 9.30 PM</p>
+						</div>
+						<p className='text-lg font-bold'>LIÊN HỆ VỚI CHÚNG TÔI</p>
+						<div className='flex flex-row gap-5 mt-3'>
+							<img src={facebookIcon} alt='facebook icon' />
+							<img src={zaloIcon} alt='zalo icon' />
+							<img src={instagramIcon} alt='instagram icon' />
+							<img src={gmailIcon} alt='gmail icon' />
+						</div>
 					</div>
-					<p className='text-lg font-bold'>LIÊN HỆ VỚI CHÚNG TÔI</p>
-					<div className='flex flex-row gap-5 mt-3'>
-						<img src={facebookIcon} alt='facebook icon' />
-						<img src={zaloIcon} alt='zalo icon' />
-						<img src={instagramIcon} alt='instagram icon' />
-						<img src={gmailIcon} alt='gmail icon' />
-					</div>
-				</div>
 
-				<div className='flex flex-col mt-4'>
-					<p className='text-lg font-bold'>CHẤP NHẬN THANH TOÁN</p>
-					<div className='flex flex-row gap-8 mt-5'>
-						<img src={momoIcon} alt='momo icon' />
-						<img src={viettelpayIcon} alt='viettelpay icon' />
-						<img src={visaIcon} alt='visa icon' />
+					<div className='flex flex-col mt-4'>
+						<p className='text-lg font-bold'>CHẤP NHẬN THANH TOÁN</p>
+						<div className='flex flex-row gap-8 mt-5'>
+							<img src={momoIcon} alt='momo icon' />
+							<img src={viettelpayIcon} alt='viettelpay icon' />
+							<img src={visaIcon} alt='visa icon' />
+						</div>
 					</div>
-				</div>
 
-				<div className='w-[3px] h-full bg-white'></div>
+					<div className='w-[3px] h-full bg-white'></div>
 
-				<div className='flex flex-col mt-4'>
-					<p className='text-lg font-bold'>
-						4Food - Hệ thống website quản lý quán ăn
-					</p>
-					<div className='grid grid-cols-2 mt-2 gap-x-5 ml-9'>
-						<p>Điều khoản chung</p>
-						<p>Hướng dẫn đặt bàn</p>
-						<p>Điều khoản giao dịch</p>
-						<p>Tuyển dụng</p>
-						<p>Chính sách thanh toán</p>
-						<p>Liên hệ</p>
-						<p>Chính sách bảo mật</p>
+					<div className='flex flex-col mt-4'>
+						<p className='text-lg font-bold'>
+							4Food - Hệ thống website quản lý quán ăn
+						</p>
+						<div className='grid grid-cols-2 mt-2 gap-x-5 ml-9'>
+							<p>Điều khoản chung</p>
+							<p>Hướng dẫn đặt bàn</p>
+							<p>Điều khoản giao dịch</p>
+							<p>Tuyển dụng</p>
+							<p>Chính sách thanh toán</p>
+							<p>Liên hệ</p>
+							<p>Chính sách bảo mật</p>
+						</div>
+						<p className='ml-9'>Thông tin thêm</p>
 					</div>
-					<p className='ml-9'>Thông tin thêm</p>
 				</div>
 			</div>
 
@@ -61,3 +63,5 @@ export default function Footer() {
 		</div>
 	)
 }
+
+		
--- src/routes/RootRoute.jsx
+++ src/routes/RootRoute.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Homepage } from '~/pages/Homepage'
 import { DefaultLayout } from '~/layouts/DefaultLayout'
+import { UserLayout } from '~/layouts/User/UserLayout'
+import { ProfilePage } from '~/pages/User/ProfilePage'
 
 function RootRoute() {
 	return (
 		<DefaultLayout>
 			<Routes>
 				<Route path='/' element={<Homepage />} />
+				<Route path='/user' element={<UserLayout />}>
+					<Route path='profile' element={<ProfilePage />} />
+					<Route index element={<Navigate to={'profile'} replace/>} />
+				</Route>
 			</Routes>
 		</DefaultLayout>
 	)
