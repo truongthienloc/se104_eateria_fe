077baaa1802d2eade3add87002fcf29ac15b2a55
unknown <truongthienloc2@gmail.com>
2023-11-19 10:05:02 +0700

    Signup - Create signup page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { OutlinedInput, InputAdornment, IconButton, styled } from '@mui/material'
 import { VisibilityOff, Visibility } from '@mui/icons-material'
 import { AuthBar } from '~/components/AuthBar'
-import googleIcon from '~/assets/images/google.svg'
 
 const FormInput = styled(OutlinedInput)`
 	width: 300px;
@@ -23,10 +22,12 @@ const FormInput = styled(OutlinedInput)`
 	}
 `
 
-export default function LoginPage() {
-	const [showPassword, setShowPassword] = useState(false)
+export default function SignupPage() {
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRePassword, setShowRePassword] = useState(false);
 
 	const handleClickShowPassword = () => setShowPassword((show) => !show)
+    const handleClickShowRePassword = () => setShowRePassword((show) => !show)
 
 	const handleMouseDownPassword = (event) => {
 		event.preventDefault()
@@ -34,16 +35,16 @@ export default function LoginPage() {
 
 	return (
 		<div className='flex flex-row font justify-evenly py-16'>
-			<AuthBar type={'login'}/>
+			<AuthBar type={'signup'}/>
 
 			<div className='flex flex-col gap-4 p-14 bg-[#FFEFD5] border-primary border-[2px] items-center rounded-lg'>
-				<h2 className='font-bold text-2xl'>ĐĂNG NHẬP</h2>
+				<h2 className='font-bold text-2xl'>ĐĂNG KÝ</h2>
 				<div className='flex flex-col gap-1'>
-					<label htmlFor='phone-number-input'>Số điện thoại *</label>
+					<label htmlFor='phone-number-input'>Vui lòng nhập số điện thoại *</label>
 					<FormInput id='phone-number-input' color='primary' placeholder='090xxxx'/>
 				</div>
 				<div className='flex flex-col gap-1'>
-					<label htmlFor='password-input'>Mật khẩu *</label>
+					<label htmlFor='password-input'>Nhập mật khẩu *</label>
 					<FormInput
 						id='password-input'
 						type={showPassword ? 'text' : 'password'}
@@ -62,26 +63,31 @@ export default function LoginPage() {
 							</InputAdornment>
 						}
 					/>
-					<a href="/forgot-password"
-						className='text-second hover:text-second hover:opacity-60'
-					>
-						Quên mật khẩu?
-					</a>
 				</div>
-
-				<div className="w-full flex flex-row gap-2 items-center">
-					<input type="checkbox" name="remember-me" id="remember-me" 
-						className='w-5 h-5 cursor-pointer'
+                <div className='flex flex-col gap-1'>
+					<label htmlFor='re-password-input'>Xác nhận lại mật khẩu *</label>
+					<FormInput
+						id='re-password-input'
+						type={showRePassword ? 'text' : 'password'}
+						sx={{letterSpacing: '0.1rem'}}
+						color='primary'
+						placeholder=''
+						endAdornment={
+							<InputAdornment position='end'>
+								<IconButton
+									aria-label='toggle password visibility'
+									onClick={handleClickShowRePassword}
+									onMouseDown={handleMouseDownPassword}
+									edge='end'>
+									{showRePassword ? <VisibilityOff /> : <Visibility />}
+								</IconButton>
+							</InputAdornment>
+						}
 					/>
-					<label htmlFor="remember-me" className='cursor-pointer'>Remember me</label>
 				</div>
 
-				<button className='px-6 py-3 bg-primary text-white text-lg'>
-					Đăng nhập
-				</button>
-				<button className='w-full flex flex-row items-center justify-center gap-5 font-bold text-lg border-primary'>
-					<img src={googleIcon} alt="google icon" />
-					<p>Đăng nhập với Google</p>
+				<button className='mt-8 px-8 py-3 bg-primary text-white text-lg'>
+					Đăng ký
 				</button>
 			</div>
 		</div>
