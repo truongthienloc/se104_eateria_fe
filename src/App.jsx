f262e1653bad30b62f0de7c3fcd81a5130b82098
unknown <truongthienloc2@gmail.com>
2023-11-28 22:45:45 +0700

    Set up - Add Date picker libraries

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react'
 import { defaultTheme } from './configs/theme'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import RootRoute from './routes/RootRoute'
 
 function App() {
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<BrowserRouter>
-				<RootRoute />
-			</BrowserRouter>
-		</ThemeProvider>
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<ThemeProvider theme={defaultTheme}>
+				<BrowserRouter>
+					<RootRoute />
+				</BrowserRouter>
+			</ThemeProvider>
+		</LocalizationProvider>
 	)
 }
 
