4742478698176713b28decd5a6dd21f6fe6b01e2
nHuy101 <nhathuyho2k3@gmail.com>
2023-12-03 14:07:06 +0700

    Add function to productdetail

--- src/components/ProductDetail_Review/FoodReview.jsx
+++ src/components/ProductDetail_Review/FoodReview.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { FaStar } from 'react-icons/fa'
+import Rating from '@mui/material/Rating'
 
 function FoodReview(props) {
 	const [rating, setRating] = useState(null)
@@ -11,32 +11,7 @@ function FoodReview(props) {
 			<div className='ml-5 my-3'>
 				<p className='text-second'>Nguyễn Văn A</p>
 			</div>
-			<div className='flex flex-row pb-10'>
-				{[...Array(5)].map((star, index) => {
-					const currentRating = index + 1
-					return (
-						<label>
-							<input
-								className='hidden'
-								type='radio'
-								name='rating'
-								value={currentRating}
-								onClick={() => setRating(currentRating)}
-							/>
-							<FaStar
-								size={24}
-								color={
-									currentRating <= (hover || rating)
-										? '#ffc107'
-										: '#e4e5e9'
-								}
-								onMouseEnter={() => setHover(currentRating)}
-								onMouseLeave={() => setHover(null)}
-							/>
-						</label>
-					)
-				})}
-			</div>
+			<Rating name='read-only' value={5} readOnly />
 			<div className='ml-5 my-3'>
 				<p className='text-second'>
 					Bún bò Huế - món ăn thơm ngon, hấp dẫn của xứ Huế.
