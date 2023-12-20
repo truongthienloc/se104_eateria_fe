1da8f2f43b20f75a2e91059bcb186b3d9e716db0
nHuy101 <nhathuyho2k3@gmail.com>
2023-12-21 00:30:07 +0700

    product search function

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -25,7 +25,11 @@ export const ProductPage = () => {
 
 	const handleSearch = () => {
 		if (!inputValue?.trim()) return;
-		setSearchParams({q: inputValue})
+		setSearchParams({keyword: inputValue})
+	}
+	const onKeyDown = (e) => {
+		if (e.key !== 'Enter') return;
+		handleSearch()
 	}
 	const handlePrice = (e) => {
 		setPrice(e.target.value)
@@ -34,12 +38,10 @@ export const ProductPage = () => {
 		setCategory(e.target.value)
 	}
 	useEffect(() => {
-		console.log('i',inputValue)
-		console.log('s',searchParams.get('q'))
-		if (inputValue === searchParams.get('q')) return;
-		console.log('here');
-		setInputValue(searchParams.get('q'));
-	}, [searchParams.get('q')]);
+		if (inputValue === searchParams.get('keyword')) return;
+		setInputValue(searchParams.get('keyword'));
+	}, [searchParams.get('keyword')]);
+
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -53,27 +55,22 @@ export const ProductPage = () => {
 				console.log(error)
 			}
 		}
-		fetchData()
 		const fetchSearchInput = async (search) =>{
 			try {
-				const res = await api.get('/dish/all/search',
-				new URLSearchParams({
-					'keyword': {search},
-				}), {
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded'
-					  }
-				})
+				const res = await api.get('/dish/all/search',{params: { 'keyword' : search}})
 				setfirstList(res.data.data)
+				setsecondList([])
 				console.log(res.data);
 			} catch (error) {
 				console.log(error);
 			}
 		}
-		const search = searchParams.get('q');
+		const search = searchParams.get('keyword');
+		console.log('search',search);
 		search?.trim() ? fetchSearchInput(search) : fetchData();
-	}, [searchParams.get('q')])
+	}, [searchParams.get('keyword')])
 
+	console.log('1stlist',firstList);
 	return (
 		<div className='flex-1 flex-col justify-center'>
 			<div>
@@ -91,6 +88,7 @@ export const ProductPage = () => {
 							placeholder='Bạn muốn tìm món gì?'
 							value={inputValue}
 							onChange={handleChange}
+							onKeyDown={onKeyDown}
 						/>
 						<FaSearch 
 						className='mx-8 cursor-pointer text-xl text-second'
@@ -127,7 +125,7 @@ export const ProductPage = () => {
 
 				<div className='max-w-[1400px] mx-auto my-10 flex flex-wrap gap-12 items-center justify-center'>
 					{firstList.length > 0 &&
-						data.map((item) => {
+						firstList.map((item) => {
 							return <FoodItems key={item.id} item={item} />
 						})}
 				</div>
@@ -141,7 +139,7 @@ export const ProductPage = () => {
 				</div>
 				<div className='max-w-[1400px] mx-auto my-10 flex flex-wrap gap-12 items-center justify-center'>
 					{secondList.length > 0 &&
-						data.map((item) => {
+						secondList.map((item) => {
 							return <FoodItems key={item.id} item={item} />
 						})}
 				</div>
